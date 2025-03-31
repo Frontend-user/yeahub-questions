d4955dad65c7d1cec8fc07938fa86cf8d6d56efd
Robert <robersargsyan2023@gmail.com>
2025-03-31 03:40:01 +0300

    ts

--- src/entities/questions/index.ts
+++ src/entities/questions/index.ts
@@ -2,3 +2,5 @@ export {useGetQuestionsQuery, questionsApi} from "@/entities/questions/api/quest
 export {questionsSlice} from "@/entities/questions/model/questionsSlice.ts";
 
 export {addQuestion} from "@/entities/questions/model/questionsSlice.ts";
+
+export type {IQuestion, ICreatedBy, ISpecialization} from "./model/types.ts";
---
+++ src/entities/questions/model/types.ts
@@ -0,0 +1,46 @@
+interface ICreatedBy {
+    userId: string;
+    firstName: string;
+    lastName: string;
+}
+
+interface ISpecialization {
+    id: number;
+    title: string;
+    description: string;
+    imageSrc: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+interface IQuestionSkill {
+    id: number;
+    title: string;
+    description: string;
+    imageSrc: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+    specializations: ISpecialization[];
+}
+
+
+interface IQuestion {
+    id: number;
+    title: string;
+    description: string;
+    code?: any | null;
+    imageSrc?: string | null;
+    keywords?: string[];
+    longAnswer: string;
+    shortAnswer: string;
+    status: string;
+    rate: number;
+    complexity: number;
+    createdAt: Date | null;
+    updatedAt: Date | null;
+    createdBy: ICreatedBy;
+    updatedBy: ICreatedBy;
+    questionSpecializations: ISpecialization[];
+    questionSkills: IQuestionSkill[]
+}
+export type {IQuestion, ICreatedBy, ISpecialization};
