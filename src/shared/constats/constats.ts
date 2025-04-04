9c2aa1b92e5fa6d7af3cd49a426d8a77b20ce9f1
Robert <robersargsyan2023@gmail.com>
2025-04-04 17:28:13 +0300

    refactor

--- src/shared/constats/constats.ts
+++ src/shared/constats/constats.ts
@@ -1,6 +1,4 @@
 export const API_TOKEN = import.meta.env.VITE_API_TOKEN
-console.log(API_TOKEN,"API_TOKEN")
-console.log(import.meta.env,"import.meta.env")
 export const API_URL = import.meta.env.VITE_API_URL
 export const API_ROUTES = {
     QUESTIONS: "questions",
